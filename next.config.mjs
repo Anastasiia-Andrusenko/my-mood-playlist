324950037459ade08c651ad77649c98c3ec05d66
Anastasiia Andrusenko <107683074+Anastasiia-Andrusenko@users.noreply.github.com>
2024-05-15 15:28:48 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   distDir: "out",
+  trailingSlash: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/login": { page: "/login" },
+      "/register": { page: "/register" },
+    };
+  },
 };
 
 export default nextConfig;
