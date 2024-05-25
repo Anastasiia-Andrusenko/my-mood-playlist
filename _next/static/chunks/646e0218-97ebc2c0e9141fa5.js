6ef8a896682ff350ab08676046e8389450776056
Anastasiia-Andrusenko <Anastasiia-Andrusenko@users.noreply.github.com>
2024-05-25 15:59:19 +0000

    deploy: a712d2f49b55a0771d51a6a634b4e88fbb4e76c0

---
+++ _next/static/chunks/646e0218-97ebc2c0e9141fa5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[123],{3804:function(t,n,e){e.d(n,{OUs:function(){return r}});var i=e(5955);function r(t){return(0,i.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",strokeWidth:"2",d:"M1,11 L12,2 L23,11 M15,23 L15,15 L15,15 L9,15 L9,23 M4,23 L4,9 M20,23 L20,9"},child:[]}]})(t)}}}]);
