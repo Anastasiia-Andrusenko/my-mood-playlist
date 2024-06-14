247adb9d9d29c6a2d5234d22bfd526c574e41611
Anastasiia Andrusenko <107683074+Anastasiia-Andrusenko@users.noreply.github.com>
2024-06-14 21:52:20 +0300

    Update Register.tsx

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -115,6 +115,7 @@ const Register: React.FC = () => {
       </p>
       <div>
         <AuthButton onClick={loginWithGoogle} text="Log in with Google" />
+        {/* hidden fb login */}
         {/* <AuthButton onClick={loginWithFacebook} text="Log in with Facebook" /> */}
       </div>
       </div>
